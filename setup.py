fbe2e9b532e1f9bc705c8db4dc5765fbb6b9f6a3
pedrobranko <49735925+pedrobranko@users.noreply.github.com>
2019-04-18 07:21:21 -0300

    setup
    Consegue ler um arquivo e criar uma chave RSA caso ele não possua

--- setup.py
+++ setup.py
@@ -1,12 +1,35 @@
-from gerador_key import (keyrsa,lista_primos) #Funções de criptografia e descriptografia
+from RSA import (keyrsa,lista_primos,encrypt,decrypt) #Funções de criptografia e descriptografia
 
-string_enter = 'mafagafo'
-string_list = [ord(a) for a in string_enter]
-chaves = keyrsa(lista_primos(200))
-string_crypt = [string_list[a]**chaves[1]%chaves[0] for a in range(len(string_list))]
-string_decrypt = [string_crypt[a]**chaves[2]%chaves[0] for a in range(len(string_list))]
-string_decrypt = [chr(a) for a in string_decrypt]
-print(chaves)
+log = open('log.txt','r')
+content = log.readlines()
+try:
+    chaves = content[0].split(',')
+    chaves = [int(chaves[i]) for i in range(len(chaves))]    
+except:
+    print('Nenhuma chave válida foi encontrada, deseja incerir uma nova? [s/n]: ')
+    switch = input()
+    while switch.upper() != 'S' and 'N':
+        print('Resposta inválida')
+        switch = input()        
+    while True:
+        if switch.upper() == 'S':
+            chave = input('Entre com chaves válidas no seguinte formato "N, Chave Pública, Chave Privada": ')
+            try:
+                chave = chave.split(',')
+                chave = (int(chave[0]),int(chave[1]),int(chave[2]))            
+                while decrypt(encrypt('a',chave),chave) != 'a':
+                    print('Chave inválida')
+                    chave = input('Entre com chaves válidas no seguinte formato "N, Chave Pública, Chave Privada": ')
+                
+            except:
+                print('Formato de chave inválido asdasd')
+
+        log = open('log.txt','w')
+        log.writelines(str(chave[0])+','+str(chave[1])+','+str(chave[0]))
+        log.close()
+        break
+    
 
 
-
+    
+    
